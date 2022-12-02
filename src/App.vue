5d812c32caed0b182fb4bcaa6421f293a91272cb
AWNY-ABOUGHALY <awny.h.ghaly@gmail.com>
2022-12-02 23:18:39 +0200

    original file task

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import Employees from './components/Employees.vue'
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    </header>
+
+    <div class="Employees">
+      <Employees />
+    </div>
+  </div>
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.container {
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 0 1rem;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    justify-content: center;
+    background: #ccc;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Employees.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-center">
+          Name
+        </th>
+        <th class="text-center">
+          Email
+        </th>
+        <th class="text-center">
+          performance
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in desserts"
+        :key="item.name"
+      >
+        <td class="text-center">{{ item.name }}</td>
+        <td class="text-center">{{ item.email }}</td>
+        <td class="text-center">{{ item.performance }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            email: 'frozen_mueller@zesla.com',
+            performance: '10%',
+          },
+          {
+            name: 'Moses Mueller',
+            email: 'moses_mueller@zesla.com',
+            performance: '70%',
+          },
+          {
+            name: 'Durham Ross',
+            email: 'durham_ross@zesla.com',
+            performance: '30%',
+          },
+          {
+            name: 'Penny Alexander',
+            email: 'penny_alexander@zesla.com',
+            performance: '15%',
+          },
+          {
+            name: 'Rose Morrison',
+            email: 'rose_morrison@zesla.com',
+            performance: '60%',
+          },
+          {
+            name: 'Ethel Adkins',
+            email: 'ethel_adkins@zesla.com',
+            performance: '12%',
+          },
+        ],
+      }
+    },
+  }
+</script>
