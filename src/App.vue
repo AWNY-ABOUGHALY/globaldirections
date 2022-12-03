b2d5129a9e643fa05245598eec10df59729b76e6
AWNY-ABOUGHALY <awny.h.ghaly@gmail.com>
2022-12-03 07:48:18 +0200

    Change HomePage style & Add search bar

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,21 @@ import Employees from './components/Employees.vue'
     <header>
       <img alt="three-sixty logo" class="logo" src="./assets/logo.png"/>
     </header>
-
+    <div class="searchbar">
+      <font-awesome-icon icon="fa-solid fa-magnifying-glass" class="searchicon"/>
+      <input type="text" class="searchbtn" placeholder="Search Here"/>
+    </div>
     <div class="Employees">
       <Employees />
     </div>
   </div>
-  <main>
-
-  </main>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
-  padding: 1rem 0;
+  display: flex;
+  justify-content: flex-start;
+  padding: 2.5rem 0;
 }
 
 .container {
@@ -29,32 +30,48 @@ header {
   padding: 0 1rem;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.searchbar {
+  position: relative;
+  max-width: 15rem;
+  margin: 1rem 0;
+  display: flex;
+  justify-content: flex-start;
+  place-items: center;
+}
+.searchbtn {
+  width: 100%;
+  background-color: #fffae6;
+  border-radius: 1.5rem;
+  padding: 0.7rem 0.7rem 0.7rem 2.7rem;
+}
+.searchbtn::placeholder {
+  font-size: 12px;
+  font-weight: bold;
+  color: #001658;
+}
+.searchbtn:focus {
+  outline: none;
+}
+.searchicon {
+  position: absolute;
+  margin-left: 1rem;
+  color: #001658;
+  font-size: 20px;
 }
 
-@media (min-width: 1024px) {
-  header {
-   /* Better to move these flex code to the header in global and remove it from here
-      then also remove the margin of logo from global and media query, so we will just have 
-      one code for centering */
-    display: flex;
-    place-items: center;
-    justify-content: center;
-    background: #ccc;
-  }
-
-  .logo {
-    /* I think it supposed to be (margin: 0 0 2rem 0;)
-       that means margin-bottom: 2rem; */
-    margin: 0 2rem 0 0;
-  }
-
+/* @media (min-width: 1024px) {
   header .wrapper {
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
+  }
+} */
+@media (max-width: 575px) {
+  header {
+    justify-content: center;
+  }
+  .searchbar {
+    max-width: 100%;
   }
 }
 </style>
